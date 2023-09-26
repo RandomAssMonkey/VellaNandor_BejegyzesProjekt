bc5af95e6411bb735a5c7c2e901738048f4571fa
Vella Nándor <nandorvella@gmail.com>
2023-09-26 09:47:47 +0200

    SzamBekeres() függvény megírása.

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -9,9 +9,29 @@ namespace BejegyzesProjekt
 {
     class Program
     {
+        public List<Bejegyzes> bejegyzes;
+
+        static void SzamBekeres()
+        {
+            Console.Write("Hány darab bejegyzést szeretnél írni? ");
+            int darabSzam = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < darabSzam; i++)
+            {
+                BejegyzesKeszites();
+            }
+
+        }
+
+        static void BejegyzesKeszites()
+        {
+            Console.WriteLine("X");
+        }
+
         static void Main(string[] args)
         {
-            
+            SzamBekeres();
+
+            Console.ReadKey();
         }
     }
 }
